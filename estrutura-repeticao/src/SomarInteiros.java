48e8857bbd61e822672cc7cb3e3853bb6e4701b5
MadJhonny <joaogepe@gmail.com>
2025-02-27 14:26:18 -0300

    Somar inteiros de 1 a 100

---
+++ estrutura-repeticao/src/SomarInteiros.java
@@ -0,0 +1,11 @@
+public class SomarInteiros {
+
+    public static void main(String[] args) {
+          int soma = 0;
+        for (int numero = 1; numero <= 100; numero ++) {
+            soma += numero;
+            System.out.println(soma);
+        }
+    }
+    
+}
