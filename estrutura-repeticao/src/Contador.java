f7452290365c9ee7a51014f4bd2cc27f949f1697
MadJhonny <joaogepe@gmail.com>
2025-02-27 14:18:16 -0300

    Contador simples utliziando "for"

---
+++ estrutura-repeticao/src/Contador.java
@@ -0,0 +1,12 @@
+public class Contador {
+    public static void main(String[] args) {
+
+        for(int contador = 0; contador <= 100; contador ++) {
+            System.out.println(contador);
+        
+        
+        }
+
+    }
+    
+}
